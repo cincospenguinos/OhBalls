8db4d7992f2b09c12dda25603ad9a08eb1e4f436
Andre LaFleur <andre.lafleur@utah.edu>
2016-02-15 21:27:54 -0700

    Collision detection figured out.

--- javascript/controller.js
+++ javascript/controller.js
@@ -6,6 +6,20 @@ var ballElements = []; // The "view" balls - the elements that represent each of
 
 var animationID; // The animation ID
 
+function ballsCollide(ball1, ball2, diameter1, diameter2){
+	var xM1 = ball1.xPos + diameter1 / 2;
+	var yM1 = ball1.yPos + diameter1 / 2;
+	var xM2 = ball2.xPos + diameter2 / 2;
+	var yM2 = ball2.yPos + diameter2 / 2;
+
+	var deltaX = Math.abs(xM2 - xM1);
+	var deltaY = Math.abs(yM2 - yM1);
+
+	var distance = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
+
+	return distance < (diameter1 / 2 + diameter2 / 2);
+}
+
 /* Adds a ball to the game/DOM */
 function addBall(){
 	var ball = new Ball(300, 250);
@@ -25,6 +39,8 @@ $(document).ready(function(){
     $('#startGame').unbind().click(function(){
 		function animate(){
 			// Make everything move
+			player.move(600, 500); // TODO: Adjustable sizes?
+
 			for(var i = 0; i < balls.length; i++){
 				var ball = balls[i];
 				var ballElement = ballElements[i];
@@ -34,15 +50,25 @@ $(document).ready(function(){
 				ballElement.css('top', ball.yPos);
 			}
 
-			player.move(600, 500); // TODO: Adjustable sizes?
-
 			// Now show that on screen for the player
 			playerElement.css('left', player.xPos);
 			playerElement.css('top', player.yPos);
+
+			// Now let's see what collides
+
+			for(var i = 0; i < balls.length; i++){
+				var ball = balls[i];
+
+				if(ballsCollide(ball, player, 50, 30)){
+					ballElements[i].css('background-color', 'red');
+				} else {
+					ballElements[i].css('background-color', 'darkgreen');
+				}
+			}
 		}
 
 		addBall();
-		addBall();
+		//addBall();
 
 		animationID = setInterval(animate, 17);
     });
